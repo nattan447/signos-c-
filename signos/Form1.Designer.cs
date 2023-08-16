ccf75836aa260b328129da03c061ee903bb05c42
nattan447 <nattanferreira58@gmail.com>
2023-08-16 15:25:40 -0300

    Add project files.

---
+++ signos/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace signos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(239, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "digite seu nascimento";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(487, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(146, 171);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(170, 108);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(354, 78);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 4;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(410, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ signos/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace signos
+{
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            string inputDate = maskedTextBox1.Text;
+            string format = "dd/MM/yyyy";
+            if (DateTime.TryParseExact(inputDate, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data))
+            {
+                if (data.Day >=21 && data.Month==3 || data.Day<=21 && data.Month==4 )
+                {
+                    pictureBox1.Image = Properties.Resources.aries;
+                }
+                else if (data.Day >= 21 && data.Month == 4 || data.Day <= 20 && data.Month == 5)
+                {
+                    pictureBox1.Image = Properties.Resources.touro;
+                }
+                else if (data.Day >= 21 && data.Month == 5 || data.Day <= 20 && data.Month == 6)
+                {
+                    pictureBox1.Image = Properties.Resources.gemeos;
+                }
+                else if (data.Day >= 21 && data.Month == 6 || data.Day <= 21 && data.Month == 7)
+                {
+                    pictureBox1.Image = Properties.Resources.cancer;
+                }
+                else if (data.Day >= 22 && data.Month == 7 || data.Day <= 22 && data.Month == 8)
+                {
+                    pictureBox1.Image = Properties.Resources.leao;
+                }
+                else if (data.Day >= 23 && data.Month == 8 || data.Day <= 22 && data.Month == 9)
+                {
+                    pictureBox1.Image = Properties.Resources.virgem;
+                }
+                else if (data.Day >= 23 && data.Month == 9 || data.Day <= 22 && data.Month == 10)
+                {
+                    pictureBox1.Image = Properties.Resources.libra;
+                }
+                else if (data.Day >= 23 && data.Month == 10 || data.Day <= 21 && data.Month == 11)
+                {
+                    pictureBox1.Image = Properties.Resources.escorpiao;
+                }
+                else if (data.Day >= 22 && data.Month == 11 || data.Day <= 21 && data.Month == 12)
+                {
+                    pictureBox1.Image = Properties.Resources.sagitario;
+                }
+                else if (data.Day >= 22 && data.Month == 12 || data.Day <= 20 && data.Month == 1)
+                {
+                    pictureBox1.Image = Properties.Resources.capricornio;
+                }
+                else if (data.Day >= 21 && data.Month == 1 || data.Day <=19 && data.Month == 2)
+                {
+                    pictureBox1.Image = Properties.Resources.aquario;
+                }
+                else if (data.Day >= 20 && data.Month == 2 || data.Day <= 20 && data.Month == 3)
+                {
+                    pictureBox1.Image = Properties.Resources.peixes;
+                }
+            }
+            else
+            {
+                MessageBox.Show("valor invalido");
+            }
+
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+     
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
