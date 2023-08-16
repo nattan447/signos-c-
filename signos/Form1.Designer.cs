22a1918e07bbc62f2e87a1fa15ec602a99dd00a9
nattan447 <nattanferreira58@gmail.com>
2023-08-16 15:40:48 -0300

    att

--- signos/Form1.Designer.cs
+++ signos/Form1.Designer.cs
@@ -32,7 +32,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,7 +51,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "resultado";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -74,20 +74,21 @@
             this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
             this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
             // 
-            // textBox1
+            // label2
             // 
-            this.textBox1.Location = new System.Drawing.Point(410, 121);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 5;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(796, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.maskedTextBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
@@ -107,7 +108,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- signos/Form1.cs
+++ signos/Form1.cs
@@ -29,56 +29,68 @@ namespace signos
 
 
 
-            string inputDate = maskedTextBox1.Text;
-            string format = "dd/MM/yyyy";
-            if (DateTime.TryParseExact(inputDate, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data))
-            {
+            string inputDate = maskedTextBox1.Text;//pega o conteúdo da markedtextbox
+            string format = "dd/MM/yyyy";//essa variável sera usada para armezar um formato especifico de data
+            if (DateTime.TryParseExact(inputDate, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data))//nesse código eu uso o método TryParseExact, que basicamente converte o input na da maskedbox com base na variável format, se o processo for sucesso ele me retorna uma variável data com a data no formato pronto, se não o programa dos signos não roda 
+            {//aqui dentro são as condicionais que determinam o dia e mês para cada signo
                 if (data.Day >=21 && data.Month==3 || data.Day<=21 && data.Month==4 )
                 {
+                    label2.Text = "aries";
                     pictureBox1.Image = Properties.Resources.aries;
                 }
                 else if (data.Day >= 21 && data.Month == 4 || data.Day <= 20 && data.Month == 5)
                 {
+                    label2.Text = "touro";
                     pictureBox1.Image = Properties.Resources.touro;
                 }
                 else if (data.Day >= 21 && data.Month == 5 || data.Day <= 20 && data.Month == 6)
                 {
+                    label2.Text ="gemeos";
                     pictureBox1.Image = Properties.Resources.gemeos;
                 }
                 else if (data.Day >= 21 && data.Month == 6 || data.Day <= 21 && data.Month == 7)
                 {
+                    label2.Text = "cancer";
                     pictureBox1.Image = Properties.Resources.cancer;
                 }
                 else if (data.Day >= 22 && data.Month == 7 || data.Day <= 22 && data.Month == 8)
                 {
+                    label2.Text = "leão";
                     pictureBox1.Image = Properties.Resources.leao;
                 }
                 else if (data.Day >= 23 && data.Month == 8 || data.Day <= 22 && data.Month == 9)
                 {
+                    label2.Text = "virgem";
                     pictureBox1.Image = Properties.Resources.virgem;
                 }
                 else if (data.Day >= 23 && data.Month == 9 || data.Day <= 22 && data.Month == 10)
                 {
+                    label2.Text = "libra";
                     pictureBox1.Image = Properties.Resources.libra;
                 }
                 else if (data.Day >= 23 && data.Month == 10 || data.Day <= 21 && data.Month == 11)
                 {
+                    label2.Text = "escorpião";
                     pictureBox1.Image = Properties.Resources.escorpiao;
                 }
                 else if (data.Day >= 22 && data.Month == 11 || data.Day <= 21 && data.Month == 12)
                 {
+                    label2.Text = "sagitário";
                     pictureBox1.Image = Properties.Resources.sagitario;
                 }
                 else if (data.Day >= 22 && data.Month == 12 || data.Day <= 20 && data.Month == 1)
                 {
+                    label2.Text = "capricórnio";
                     pictureBox1.Image = Properties.Resources.capricornio;
                 }
                 else if (data.Day >= 21 && data.Month == 1 || data.Day <=19 && data.Month == 2)
                 {
+                    label2.Text = "aquário";
                     pictureBox1.Image = Properties.Resources.aquario;
                 }
                 else if (data.Day >= 20 && data.Month == 2 || data.Day <= 20 && data.Month == 3)
                 {
+                    label2.Text = "peixes";
                     pictureBox1.Image = Properties.Resources.peixes;
                 }
             }
@@ -104,6 +116,11 @@ namespace signos
         private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
         {
 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
